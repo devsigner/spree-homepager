304c4e719dff66c556a422a7f82829d80cb9e518
Cedric Darricau <cedric@avantagegraphique.net>
2012-03-30 17:35:53 +0200

    add thumbnail large size

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,6 +1,6 @@
 class Banner < ActiveRecord::Base
   acts_as_list
-  has_attached_file :image, :styles => {:thumb => "100x100>" }
+  has_attached_file :image, :styles => {:thumb => "100x100>", :large => "685x288#" }
   default_scope :order => "position"
 
   scope :visible, where(:visible => true)
